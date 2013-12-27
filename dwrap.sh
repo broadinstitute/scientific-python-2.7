e7ba191b9a1c7a34192c44d0a8e8d6375e63eb90
root <root@static.148-251-3-162.clients.your-server.de>
2013-12-27 19:28:05 +0100

    fix copy order

--- dwrap.sh
+++ dwrap.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+#
+# Copy intel mkl shared libs from compiler install
+#
+mkdir intel
 cp -R /opt/intel/composerxe/mkl/lib/intel64/* intel
 cp /opt/intel/composerxe/mkl/lib/mic/*scala* intel
-mkdir intel
+
+# 
+# Run the Docker build
+#
 docker build -t scivm/scientific-python-2.7 .
